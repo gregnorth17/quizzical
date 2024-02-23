952ca3e0ab615a5508ecc6f210b77302fc0c6f5e
gregnorth17 <greg_north@hotmail.co.uk>
2024-02-23 12:42:10 -0600

    answers

--- src/Question.jsx
+++ src/Question.jsx
@@ -1,14 +1,18 @@
 import { decode } from 'html-entities'
+import { nanoid } from 'nanoid'
 
 const Question = ({questionData}) => {
   console.log(questionData)
     // PUt all answers in same array
-    const answers = 
+    const answers = [...questionData.incorrect_answers, questionData.correct_answer].sort()
+    console.log(answers)
+
     return (
       <div className='question'>
         <h3 className='question-title'>{decode(questionData.question)}</h3>
-        {/* {questionHTML} */}
-        
+        <div className='answers'>
+          {answers.map(answer => <span className='answer' key={nanoid()}>{answer}</span>)}
+        </div>
       </div>
     )
  
